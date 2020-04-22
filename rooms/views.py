684608064b9fe43572a7e3d07b64a1fb647595af
loshy <loshy244110@gmail.com>
2020-04-22 22:40:19 +0900

    feat: add page and try, exception

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,22 @@
-from math import ceil
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/all_rooms.html", {"page": rooms},)
+    except EmptyPage:
+        rooms = paginator.page(1)
+        return redirect("/")
+    except InvalidPage:
+        rooms = paginator.page(1)
+        return redirect("/")
+    except Exception:
+        rooms = paginator.page(1)
+        return redirect("/")
     print(vars(rooms.paginator))
-    return render(request, "rooms/all_rooms.html", {"rooms": rooms},)
