375c2be13ccf6437b809367a84fb9ec6c92192e5
loshy <loshy244110@gmail.com>
2020-04-25 19:12:24 +0900

    feat: add ListView

--- rooms/views.py
+++ rooms/views.py
@@ -1,22 +1,10 @@
-from django.shortcuts import render, redirect
+from django.views.generic import ListView
 from . import models
-from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/all_rooms.html", {"page": rooms},)
-    except EmptyPage:
-        rooms = paginator.page(1)
-        return redirect("/")
-    except InvalidPage:
-        rooms = paginator.page(1)
-        return redirect("/")
-    except Exception:
-        rooms = paginator.page(1)
-        return redirect("/")
-    print(vars(rooms.paginator))
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
