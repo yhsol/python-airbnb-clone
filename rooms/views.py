8cb0ba4070833c19cfa01f8079783bfc8d3a8680
loshy <loshy244110@gmail.com>
2020-04-25 19:31:01 +0900

    feat: add context to ListView

--- rooms/views.py
+++ rooms/views.py
@@ -8,3 +8,8 @@ class HomeView(ListView):
 
     model = models.Room
     paginate_by = 10
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
