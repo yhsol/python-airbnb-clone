1c2c8a0a86f3620b89c10d9b7fed58ce40f3ca21
loshy <loshy244110@gmail.com>
2020-04-26 20:15:08 +0900

    feat: add search with form

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from django_countries import countries
+
 from . import models
 
 
@@ -20,6 +22,44 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    country = request.GET.get("country", "kr")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    geusts = int(request.GET.get("geusts", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    amenities = request.GET.getlist("amenities")
+    facilities = request.GET.getlist("facilities")
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+
+    form = {
+        "city": city,
+        "selected_country": country,
+        "selected_room_type": room_type,
+        "price": price,
+        "geusts": geusts,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "selected_amenities": amenities,
+        "selected_facilities": facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(request, "rooms/search.html", {**form, **choices},)
