7ef739ec64a634375c0c3d7faaa87e7f35a35948
loshy <loshy244110@gmail.com>
2020-05-09 22:23:45 +0900

    feat: signup with ModelForm

--- users/forms.py
+++ users/forms.py
@@ -21,22 +21,14 @@ class LoginForm(forms.Form):
 
 
 class SignupForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
 
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     password_check = forms.CharField(
         widget=forms.PasswordInput, label="Confirm password"
     )
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists with that email")
-        except models.User.DoesNotExist:
-            return email
 
     def clean_password_check(self):
         password = self.cleaned_data.get("password")
@@ -47,12 +39,10 @@ class SignupForm(forms.ModelForm):
         else:
             return password
 
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-        user = models.User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
+        user.username = email
+        user.set_password(password)
         user.save()
