d392039e13f91fec6975956c5de0601f3103763f
loshy <loshy244110@gmail.com>
2020-05-16 19:23:25 +0900

    feat: request for github login

--- users/admin.py
+++ users/admin.py
@@ -20,6 +20,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -40,6 +41,7 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "email_verified",
         "email_secret",
+        "login_method",
     )
 
     # list_filter
