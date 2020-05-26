44a88fd1de6e4bf0fa7fc44ae3dc7df1921e06e7
loshy <loshy244110@gmail.com>
2020-05-26 20:26:34 +0900

    feat: UserCreationForm for validation

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -20,29 +21,34 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model = models.User
-        fields = ("first_name", "last_name", "email")
+# class SignupForm(forms.ModelForm):
+#     class Meta:
+#         model = models.User
+#         fields = ("first_name", "last_name", "email")
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_check = forms.CharField(
-        widget=forms.PasswordInput, label="Confirm password"
-    )
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     password_check = forms.CharField(
+#         widget=forms.PasswordInput, label="Confirm password"
+#     )
 
-    def clean_password_check(self):
-        password = self.cleaned_data.get("password")
-        password_check = self.cleaned_data.get("password_check")
+#     def clean_password_check(self):
+#         password = self.cleaned_data.get("password")
+#         password_check = self.cleaned_data.get("password_check")
 
-        if password != password_check:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
+#         if password != password_check:
+#             raise forms.ValidationError("Password confirmation does not match")
+#         else:
+#             return password
 
-    def save(self, *args, **kwargs):
-        user = super().save(commit=False)
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-        user.username = email
-        user.set_password(password)
-        user.save()
+#     def save(self, *args, **kwargs):
+#         user = super().save(commit=False)
+#         email = self.cleaned_data.get("email")
+#         password = self.cleaned_data.get("password")
+#         user.username = email
+#         user.set_password(password)
+#         user.save()
+
+
+class SignupForm(UserCreationForm):
+
+    username = forms.EmailField(label="Email")
